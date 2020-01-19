19ba7d2624b59430aeabacb25a3c3b0db7356f42
Kenta Kase <kesin1202000@gmail.com>
2020-01-20 00:03:46 +0900

    Add snapshot tests

---
+++ __tests__/snapshot.test.ts
@@ -0,0 +1,23 @@
+import * as junit2json from '../src/index'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const fixturePath = (fixtureName: string) => {
+  return path.join(__dirname, 'fixtures', fixtureName)
+}
+
+describe('parse snapshot', () => {
+  it('all success xml', async () => {
+    const xml = fs.readFileSync(fixturePath('jest-success.xml'))
+    const parsed = await junit2json.parse(xml)
+
+    expect(parsed).toMatchSnapshot()
+  })
+
+  it('failure xml', async () => {
+    const xml = fs.readFileSync(fixturePath('jest-failure.xml'))
+    const parsed = await junit2json.parse(xml)
+
+    expect(parsed).toMatchSnapshot()
+  })
+})
