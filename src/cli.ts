4ad503e0aa24fe6218deef3325deea20fc3788c5
Kenta Kase <kesin1202000@gmail.com>
2023-03-18 09:30:38 +0000

    Refactor yargs

--- src/cli.ts
+++ src/cli.ts
@@ -1,30 +1,27 @@
 #!/usr/bin/env node
 import fs from 'fs'
 import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 import { parse } from '.'
 
-type Argv = {
-  pretty: boolean
-  filterTags?: string[]
-  path: string
-}
-
-const argv = yargs
+const argv = yargs(hideBin(process.argv))
   .usage('Usage: $0 [path]')
-  .command('$0 <path>', 'Convert JUnit XML format to JSON', (yargs) => {
-    yargs.options({
-      p: { type: 'boolean', alias: 'pretty', describe: 'Output pretty JSON' },
-      f: { type: 'string', alias: 'filter-tags', describe: 'Filter XML tag names' },
-    })
-    yargs.coerce('f', (filter?: string) => filter?.split(',') )
-    yargs.positional('path', {
-      describe: 'JUnit XML path',
-      type: 'string'
-    })
-    .example('$0 -p -f system-out,system-err junit.xml', 'Output pretty JSON with filter <system-out> and <system-err> tags.')
+  .command('$0 <path>', 'Convert JUnit XML format to JSON')
+  .options({
+    p: { type: 'boolean', alias: 'pretty', describe: 'Output pretty JSON', default: false },
+    f: { type: 'string', alias: 'filter-tags', describe: 'Filter XML tag names' },
   })
+  .alias('p', 'pretty')
+  .alias('f', 'filter-tags')
+  .coerce('f', (filter?: string) => filter?.split(',') )
+  .positional('path', {
+    describe: 'JUnit XML path',
+    type: 'string',
+    demandOption: true,
+  })
+  .example('$0 -p -f system-out,system-err junit.xml', 'Output pretty JSON with filter <system-out> and <system-err> tags.')
   .strict()
-  .argv as unknown as Argv
+  .parseSync()
 
 const main = async () => {
   const xmlString = fs.readFileSync(argv.path, 'utf8').toString()
