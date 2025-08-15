c8ebb8e5ecd21e648c001ea9fa1a932d8fca65a2
Kenta Kase <kesin1202000@gmail.com>
2025-08-15 09:32:37 +0900

    Use parseArgs instead of yargs

--- src/cli.ts
+++ src/cli.ts
@@ -1,39 +1,63 @@
 #!/usr/bin/env node
 import fs from 'node:fs'
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
+import { parseArgs } from 'node:util'
 import { parse } from './index.js'
 
-const argv = yargs(hideBin(process.argv))
-  .usage('Usage: $0 [path]')
-  .command('$0 <path>', 'Convert JUnit XML format to JSON')
-  .options({
-    p: { type: 'boolean', alias: 'pretty', describe: 'Output pretty JSON', default: false },
-    f: { type: 'string', alias: 'filter-tags', describe: 'Filter XML tag names' },
-  })
-  .alias('p', 'pretty')
-  .alias('f', 'filter-tags')
-  .coerce('f', (filter?: string) => filter?.split(',') )
-  .positional('path', {
-    describe: 'JUnit XML path',
-    type: 'string',
-    demandOption: true,
-  })
-  .example('$0 -p -f system-out,system-err junit.xml', 'Output pretty JSON with filter <system-out> and <system-err> tags.')
-  .strict()
-  .parseSync()
+const printHelp = () => {
+  const help = `junit2json - Convert JUnit XML format to JSON
+
+Usage:
+  junit2json [options] <path>
+
+Options:
+  -p, --pretty              Output pretty formatted JSON (default: false)
+  -f, --filter-tags <tags>  Comma separated XML tag names to filter out from output
+  -h, --help                Show this help and exit
+
+Examples:
+  junit2json junit.xml
+  junit2json -p -f system-out,system-err junit.xml`
+  console.log(help)
+}
 
 const main = async () => {
-  const xmlString = fs.readFileSync(argv.path, 'utf8').toString()
+  const { values, positionals } = parseArgs({
+    options: {
+      help: { type: 'boolean', short: 'h' },
+      pretty: { type: 'boolean', short: 'p' },
+      'filter-tags': { type: 'string', short: 'f' },
+    },
+    allowPositionals: true,
+    strict: true,
+  })
 
-  const filterSet = new Set(argv.filterTags)
+  if (values.help) {
+    printHelp()
+    process.exit(0)
+  }
+
+  const path = positionals[0]
+  if (!path) {
+    console.error('Error: Missing required <path> to JUnit XML file.')
+    console.error('')
+    printHelp()
+    process.exit(1)
+  }
+
+  const xmlString = fs.readFileSync(path, 'utf8').toString()
+
+  const filterTags = typeof values['filter-tags'] === 'string' && values['filter-tags'].length > 0
+    ? values['filter-tags'].split(',')
+    : []
+
+  const filterSet = new Set<string>(filterTags)
   const _replacer = (key: any, value: any) => {
     if (filterSet.has(key)) return undefined
     return value
   }
 
-  const indent = (argv.pretty) ? 2 : 0
-  const replacer = (argv.filterTags && argv.filterTags.length > 0) ? _replacer : undefined
+  const indent = values.pretty ? 2 : 0
+  const replacer = (filterTags.length > 0) ? _replacer : undefined
 
   const output = await parse(xmlString)
   console.log(JSON.stringify(output, replacer, indent))
